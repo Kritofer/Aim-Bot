9424ed91aae6a5e72e4f72f98cdf952e0169d141
Kritofer <105889306+Kritofer@users.noreply.github.com>
2024-06-03 02:52:32 -0300

    Update main_code.lua

--- main_code.lua
+++ main_code.lua
@@ -51,7 +51,7 @@ end
 function shoot(player: Player)
 	local lookto = CFrame.lookAt(Camera.CFrame.Position, player.Character.Head.Position)
 	if Config.Advanced then
-		lookto = CFrame.lookAt(Camera.CFrame.Position, player.Character.Head.Position + (player.Character.HumanoidRootPart.Velocity * 0.1))
+		lookto = CFrame.lookAt(Camera.CFrame.Position + Localplayer.Character.HumanoidRootPart.Velocity * 0.3, player.Character.Head.Position + (player.Character.HumanoidRootPart.Velocity * 0.3))
 	end
 	Camera.CFrame = lookto
 end
